7ec4030ef284364341bb533fef20defe1e819eeb
Ivan Zennaro <48953048+Ivan-Zennaro@users.noreply.github.com>
2022-04-25 21:24:27 +0200

    vue

---
+++ Vue/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <notifications group="foo" position="center" />
+    <v-overlay :value="charging">
+      <v-progress-circular
+        v-if="charging"
+        indeterminate
+        color="primary"
+      ></v-progress-circular>
+    </v-overlay>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  computed: {
+    charging() {
+      return this.$store.getters.getCharging;
+    }
+  },
+  data: () => ({}),
+  methods: {}
+};
+</script>
